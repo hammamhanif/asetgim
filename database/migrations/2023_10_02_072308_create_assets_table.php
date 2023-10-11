48fa03a54853ae1c3e12dc2dd895cdbdbb53a292
hammamhanif <hammam123ahmad@gmail.com>
2023-10-11 09:33:04 +0700

    upload asset controller

--- database/migrations/2023_10_02_072308_create_assets_table.php
+++ database/migrations/2023_10_02_072308_create_assets_table.php
@@ -14,10 +14,12 @@ return new class extends Migration
         Schema::create('assets', function (Blueprint $table) {
             $table->id();
             $table->string('name');
+            $table->unsignedBigInteger('user_id');
             $table->enum('type', ['2D', '3D'])->default('2D');
             $table->enum('status', ['active', 'inactive'])->default('inactive');
+            $table->string('path');
             $table->unsignedInteger('count')->default(0);
-            $table->foreignId('user_id')->constrained();
+            $table->foreign('user_id')->references('id')->on('users')->onDelete('cascade');
             $table->timestamps();
         });
     }
